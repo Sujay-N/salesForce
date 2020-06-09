b0b78e6c9793880cf3b6ac59ca197190a01cb302
Sujay-N <sujayn18@gmail.com>
2020-06-09 11:10:48 +0530

    Updates

---
+++ precache-manifest.4a6f8e755778e2544000a54d903c1397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "921ab37d1708fe750af2ca1759054a82",
+    "url": "/salesForce/index.html"
+  },
+  {
+    "revision": "76517e0317f0bb8675a9",
+    "url": "/salesForce/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "868b42b4fd3396150591",
+    "url": "/salesForce/static/js/2.17c6052d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/salesForce/static/js/2.17c6052d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76517e0317f0bb8675a9",
+    "url": "/salesForce/static/js/main.dd826382.chunk.js"
+  },
+  {
+    "revision": "345234e5acdf7de9f8a8",
+    "url": "/salesForce/static/js/runtime-main.127830fc.js"
+  }
+]);
